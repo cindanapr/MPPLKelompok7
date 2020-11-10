59237558406583118b9271f529fffa8e89cd8e5c
Arih Ghina Wiranti <ghina_wiranti@apps.ipb.ac.id>
2020-11-11 03:16:02 +0700

    projek awal
    init project

---
+++ android/app/src/main/kotlin/com/example/mppl/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mppl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,11 @@
+import '';
+
+class AuthService {
+
+  //sign in
+
+  //register
+
+  //sign out
+
+}
